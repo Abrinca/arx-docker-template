6a22da572dddcbbeacfac6d9b9d4e65d6f38f0c4
MrTomRod <roder.thomas@gmail.com>
2022-03-15 16:17:00 +0100

    big refactoring: rename database to folder_structure to avoid confusi

--- download_demo_db.sh
+++ download_demo_db.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# download demo database
-if [[ -d ./database ]]; then
-  echo 'Folder "database" already exists. Remove it if you want to download again.'
+# download demo folder_structure
+if [[ -d ./folder_structure ]]; then
+  echo 'Folder "folder_structure" already exists. Remove it if you want to download again.'
 else
-  echo "Download demo database..."
-  curl https://cloud.bioinformatics.unibe.ch/index.php/s/eQqxxwrZWjHsDML/download -o database.tar.xz
-  echo "Unpack demo database"
-  tar -xf database.tar.xz
-  rm database.tar.xz
+  echo "Download demo folder_structure..."
+  curl https://cloud.bioinformatics.unibe.ch/index.php/s/DYNrQZZTrCtYHfW/download/folder_structure.tar.xz -o folder_structure.tar.xz
+  echo "Unpack demo folder_structure"
+  tar -xf folder_structure.tar.xz
+  rm folder_structure.tar.xz
 fi
 
 # run docker-compose
