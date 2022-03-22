3166fa12cf12fabbf1aa84ed3d70cecb9af5a11f
MrTomRod <roder.thomas@gmail.com>
2022-03-22 12:29:25 +0100

    new link without expiration date

--- download_demo_db.sh
+++ download_demo_db.sh
@@ -5,7 +5,7 @@ if [[ -d ./folder_structure ]]; then
   echo 'Folder "folder_structure" already exists. Remove it if you want to download again.'
 else
   echo "Download demo folder_structure..."
-  curl https://cloud.bioinformatics.unibe.ch/index.php/s/DYNrQZZTrCtYHfW/download/folder_structure.tar.xz -o folder_structure.tar.xz
+  curl https://cloud.bioinformatics.unibe.ch/index.php/s/HEXfpEcNSQk4jP2/download/folder_structure.tar.xz -o folder_structure.tar.xz
   echo "Unpack demo folder_structure"
   tar -xf folder_structure.tar.xz
   rm folder_structure.tar.xz
