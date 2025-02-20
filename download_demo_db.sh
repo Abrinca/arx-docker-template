bda611f98ddd18972d6e3927cded434a518ca02d
MrTomRod <roder.thomas@gmail.com>
2025-02-20 23:07:48 +0100

    cleaned up, make multiservice work

--- download_demo_db.sh
+++ download_demo_db.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # download demo folder_structure
-if [[ -d ./folder_structure ]]; then
-  echo 'Folder "folder_structure" already exists. Remove it if you want to download again.'
+if [[ -d ./ogb_container/folder_structure ]]; then
+    echo 'Folder "folder_structure" already exists. Remove it if you want to download again.'
 else
-  echo "Download demo folder_structure..."
-  curl https://cloud.bioinformatics.unibe.ch/index.php/s/HEXfpEcNSQk4jP2/download/folder_structure.tar.xz -o folder_structure.tar.xz
-  echo "Unpack demo folder_structure"
-  tar -xf folder_structure.tar.xz -C ./ogb_container/
-  rm folder_structure.tar.xz
+    echo "Download demo folder_structure..."
+    curl https://cloud.bioinformatics.unibe.ch/index.php/s/HEXfpEcNSQk4jP2/download/folder_structure.tar.xz -o folder_structure.tar.xz
+    echo "Unpack demo folder_structure"
+    mkdir -p ./ogb_container
+    tar -xf folder_structure.tar.xz -C ./ogb_container
+    rm folder_structure.tar.xz
 fi
 
 # run docker-compose
-echo "ready! run 'docker-compose -f production-template.yaml up'"
+echo "ready! run 'docker-compose -f production.yaml up'"
